8a9ce25955dfe96d741492820bcc673e3a5f75ee
Eliot Ferragni <eliot.ferragni@epfl.ch>
2017-10-30 11:00:37 +0100

    -deleted some debug messages

--- Linux_32bits/Create Eclipse_Mobots.sh
+++ Linux_32bits/Create Eclipse_Mobots.sh
@@ -19,14 +19,12 @@ echo to generate the Eclipse_Mobots folder and press [ENTER]
 
 read installation_dir
 installation_dir=$(echo $installation_dir | sed "s/'//g")
-echo $installation_dir
 
 echo
 echo Enter the path to the eclipse folder you want to use and press [ENTER]
 
 read eclipse_path
 eclipse_path=$(echo $eclipse_path | sed "s/'//g")
-echo $eclipse_path
 
 echo
 echo Enter the path to the arm-none-eabi toolchain folder you want to use
@@ -35,8 +33,6 @@ echo and press [ENTER]
 read gcc_path
 gcc_path=$(echo $gcc_path | sed "s/'//g")
 gcc_folder_name=`basename "$gcc_path"`
-echo $gcc_path
-echo $gcc_folder_name
 
 echo
 echo Copying Eclipse
